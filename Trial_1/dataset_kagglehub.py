7f6fd907295511d30f120973f795d89e97a40955
Bernadette <regislake809@gmail.com>
2026-08-24 09:05:22 +0700

    Rearange project path

--- Trial_1/dataset_kagglehub.py
+++
@@ -1,6 +0,0 @@
-import kagglehub # type: ignore
-
-# Download latest version
-path = kagglehub.dataset_download("jonathanoheix/face-expression-recognition-dataset")
-
-print("Path to dataset files:", r"C:\Florence's\Work Environtment\Magang - Imigrasi\Facial Scores\Trial_1")
